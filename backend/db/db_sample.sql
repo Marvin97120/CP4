c309808c5869c64b0f5f2b0d67636fdb7212c94d
Benoît V <benoit@vandanjon.fr>
2022-07-20 14:42:38 +0200

    duplicate personal template + setup DB with semi-samples

--- backend/db/db_sample.sql
+++ backend/db/db_sample.sql
@@ -1,19 +1,69 @@
-DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `panels`;
+DROP TABLE IF EXISTS `images`;
+DROP TABLE IF EXISTS `categories`;
 
-CREATE TABLE `users` (
+-- ------------------------------------------------------------------------- --
+
+CREATE TABLE `categories` (
   `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `firstname` VARCHAR(100) NOT NULL,
-  `lastname` VARCHAR(100) NULL,
-  `password` VARCHAR(50) NOT NULL,
-  `email` VARCHAR(255) NULL,
-  `phone` VARCHAR(20) NULL
-) ENGINE=InnoDB;
+  `name` VARCHAR(255) NOT NULL
+  ) ENGINE=InnoDB;
 
-LOCK TABLES `users` WRITE;
-INSERT INTO `users` (`firstname`, `lastname`,`password`, `email`, `phone`) VALUES
-("JohnA", "SmithA", "Iamthebest", "JohnA@company.com", "+33111111111"),
-("JohnB", "SmithB", "Iamthebest", "JohnB@company.com", "+33222222222"),
-("JohnC", "SmithC", "Iamthebest", "JohnC@company.com", "+33333333333"),
-("JohnD", "SmithD", "Iamthebest", "JohnD@company.com", "+33444444444"),
-("JohnE", "SmithE", "Iamthebest", "JohnE@company.com", "+33555555555");
+LOCK TABLES `categories` WRITE;
+INSERT INTO `categories` (`name`) VALUES
+("l'usine"),
+("les travailleurs"),
+("les productions"),
+("les K'Veguen");
+UNLOCK TABLES;
+
+-- ------------------------------------------------------------------------- --
+
+CREATE TABLE `images` (
+  `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `title` VARCHAR(255) NOT NULL,
+  `url` VARCHAR(255) NOT NULL,
+  `alt` VARCHAR(255) NOT NULL,
+  `original_src` VARCHAR(255) NOT NULL
+  ) ENGINE=InnoDB;
+
+LOCK TABLES `images` WRITE;
+INSERT INTO `files` (`title`, `url`, `alt`, `original_src`) VALUES
+("image01", "urltest", "alttest", "originalsrctest"),
+("image02", "urltest", "alttest", "originalsrctest"),
+("image03", "urltest", "alttest", "originalsrctest"),
+("image04", "urltest", "alttest", "originalsrctest"),
+("image05", "urltest", "alttest", "originalsrctest"),
+("image06", "urltest", "alttest", "originalsrctest"),
+("image07", "urltest", "alttest", "originalsrctest"),
+("image08", "urltest", "alttest", "originalsrctest"),
+("image09", "urltest", "alttest", "originalsrctest"),
+("image10", "urltest", "alttest", "originalsrctest"),
+("image11", "urltest", "alttest", "originalsrctest"),
+("image12", "urltest", "alttest", "originalsrctest");
+UNLOCK TABLES;
+
+-- ------------------------------------------------------------------------- --
+
+CREATE TABLE `panels` (
+  `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `title` VARCHAR(255) NOT NULL,
+  `main_image_id` INT NOT NULL,
+  `illus1_id` INT NULL,
+  `illus2_id` INT NULL,
+  `illus3_id` INT NULL,
+  `text` VARCHAR(255) NOT NULL,
+  `category_id` INT NOT NULL,
+  FOREIGN KEY (main_image_id) REFERENCES images(id),
+  FOREIGN KEY (illus1_id) REFERENCES images(id),
+  FOREIGN KEY (illus2_id) REFERENCES images(id),
+  FOREIGN KEY (illus3_id) REFERENCES images(id),
+  FOREIGN KEY (category_id) REFERENCES categories(id)
+  ) ENGINE=InnoDB;
+
+LOCK TABLES `panels` WRITE;
+INSERT INTO `panels` (`title`, `main_image`, `illus1`, `illus2`, `illus3`, `text`, `category`) VALUES
+("panneau01", 1, 2, 3, 4, "description panneau", 1);
+("panneau02", 5, 6, 7, 8, "description panneau", 2);
+("panneau03", 9, 10, 11, 12, "description panneau", 3);
 UNLOCK TABLES;
