1274bfb13e5f4e38b9c4f25b6ee3f74343fb64d4
Benoît V <benoit@vandanjon.fr>
2022-07-20 23:35:29 +0200

    axios get all panels on crud page

--- frontend/src/pages/CRUD.jsx
+++ frontend/src/pages/CRUD.jsx
@@ -1,13 +1,36 @@
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const CRUD = () => {
+	const [panelsList, setPanelsList] = useState();
+
+	useEffect(() => {
+		axios
+			.get("http://localhost:5000/")
+			.then((res) => {
+				setPanelsList(res.data);
+			})
+			.catch((err) => {
+				console.error(err.res.data);
+			});
+	}, []);
+
+	if (!panelsList) return "";
+
+	console.log(panelsList);
+
 	return (
 		<main id="MainCRUD">
-			<h1>CRUD</h1>
-			<p>CRUD page</p>
-			<Link to="/home">
-				<button>CLICK ME</button>
-			</Link>
+			<h1>Liste des panneaux</h1>
+
+			{panelsList.map((p, index) => (
+				<article key={index}>
+					<p>{p.panel_title}</p>
+					<p>{p.main_url}</p>
+					<p>{p.main_title}</p>
+					<p>{p.category}</p>
+				</article>
+			))}
 		</main>
 	);
 };
