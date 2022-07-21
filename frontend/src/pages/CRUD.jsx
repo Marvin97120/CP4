4e446813a80d74472073a28162cdaa4f10cdcbd5
Benoît V <benoit@vandanjon.fr>
2022-07-21 12:23:49 +0200

    correction strange return from get/all route with an ORDER BY + wrong call which was limiting panels return to 4

--- frontend/src/pages/CRUD.jsx
+++ frontend/src/pages/CRUD.jsx
@@ -5,9 +5,6 @@ import NavAdmin from "@comp/NavAdmin";
 
 const CRUD = () => {
 	const [panels, setPanels] = useState([]);
-	const [currentPanel, setCurrentPanel] = useState();
-
-	const [index, setIndex] = useState(0);
 
 	useEffect(() => {
 		axios
@@ -55,135 +52,64 @@ const CRUD = () => {
 				<h1>Ajouter un panneau</h1>
 
 				<section>
-					{index === 0 && (
-						<fieldset>
-							<legend>panneau</legend>
-							<label>Titre</label>
-							<input
-								type="text"
-								name="title"
-								value={newPanel.title}
-								onChange={handleChange}
-							/>
+					<fieldset>
+						<legend>panneau</legend>
+						<label>Titre</label>
+						<input
+							type="text"
+							name="title"
+							value={newPanel.title}
+							onChange={handleChange}
+						/>
 
-							<label>Texte</label>
-							<input
-								type="text"
-								name="text"
-								value={newPanel.text}
-								onChange={handleChange}
-							/>
+						<label>Texte</label>
+						<input
+							type="text"
+							name="text"
+							value={newPanel.text}
+							onChange={handleChange}
+						/>
 
-							<label>Catégorie</label>
-							<input
-								type="text"
-								name="category_id"
-								value={newPanel.category_id}
-								onChange={handleChange}
-							/>
+						<label>Catégorie</label>
+						<input
+							type="text"
+							name="category_id"
+							value={newPanel.category_id}
+							onChange={handleChange}
+						/>
 
-							<label>URL image principale</label>
-							<input
-								type="text"
-								name="main_image_id"
-								value={newPanel.main_image_id}
-								onChange={handleChange}
-							/>
+						<label>URL image principale</label>
+						<input
+							type="text"
+							name="main_image_id"
+							value={newPanel.main_image_id}
+							onChange={handleChange}
+						/>
 
-							<label>URL image 1</label>
-							<input
-								type="text"
-								name="illus1_id"
-								value={newPanel.illus1_id}
-								onChange={handleChange}
-							/>
+						<label>URL image 1</label>
+						<input
+							type="text"
+							name="illus1_id"
+							value={newPanel.illus1_id}
+							onChange={handleChange}
+						/>
 
-							<label>URL image 2</label>
-							<input
-								type="text"
-								name="illus2_id"
-								value={newPanel.illus2_id}
-								onChange={handleChange}
-							/>
+						<label>URL image 2</label>
+						<input
+							type="text"
+							name="illus2_id"
+							value={newPanel.illus2_id}
+							onChange={handleChange}
+						/>
 
-							<label>URL image 3</label>
-							<input
-								type="text"
-								name="illus3_id"
-								value={newPanel.illus3_id}
-								onChange={handleChange}
-							/>
-						</fieldset>
-					)}
-					{index === 1 && (
-						<fieldset>
-							<legend>image principale</legend>
-							<label>Titre</label>
-							<input type="text" />
-
-							<label>Source du site</label>
-							<input type="text" />
-
-							<label>Texte alternatif</label>
-							<input type="text" />
-						</fieldset>
-					)}
-
-					{index === 2 && (
-						<fieldset>
-							<legend>illustration 1</legend>
-							<label>Titre</label>
-							<input type="text" />
-
-							<label>Source du site</label>
-							<input type="text" />
-
-							<label>Texte alternatif</label>
-							<input type="text" />
-						</fieldset>
-					)}
-
-					{index === 3 && (
-						<fieldset>
-							<legend>illustration 2</legend>
-							<label>Titre</label>
-							<input type="text" />
-
-							<label>Source du site</label>
-							<input type="text" />
-
-							<label>Texte alternatif</label>
-							<input type="text" />
-						</fieldset>
-					)}
-
-					{index === 4 && (
-						<fieldset>
-							<legend>illustration 3</legend>
-							<label>Titre</label>
-							<input type="text" />
-
-							<label>Source du site</label>
-							<input type="text" />
-
-							<label>Texte alternatif</label>
-							<input type="text" />
-						</fieldset>
-					)}
-
-					{index > 0 && (
-						<button onClick={() => setIndex(index - 1)}>Précédent</button>
-					)}
-
-					{index < 4 && (
-						<button onClick={() => setIndex(index + 1)}>Suivant</button>
-					)}
-
-					{index === 4 && (
-						<button onClick={() => setIndex(index + 1)}>
-							Valider le formulaire
-						</button>
-					)}
+						<label>URL image 3</label>
+						<input
+							type="text"
+							name="illus3_id"
+							value={newPanel.illus3_id}
+							onChange={handleChange}
+						/>
+					</fieldset>
 
 					<button onClick={() => addPanel(newPanel)}>
 						Valider le formulaire
